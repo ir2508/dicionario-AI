3a6df94d74cc0d81436ee128c416b19f1d196364
Igor Roberto <47528721+ir2508@users.noreply.github.com>
2026-03-25 19:40:11 +0000

    Criação dos primeiros componentes e instalação do axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <main className='container'>
+        <div className='row'>
+          <div className='col-12 text-center mt-5 mb-5'>
+
+        <button className='btn btn-success'>Novas Palavras</button>
+          </div>
+        </div>
+
+        <section className='row'>
+          <Card></Card>  
+          <Card></Card>  
+          <Card></Card>  
+          <Card></Card>  
+          <Card></Card>  
+        </section>
+    </main>
   )
 }
 
